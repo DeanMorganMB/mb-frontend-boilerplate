3896995051dfc3063e45510d7e4a985864baa1ed
DeanMorganMB <deanmorgan@mediablox.co.uk>
2019-05-20 11:10:12 +0100

    Merged changes from original source

--- config.js
+++ config.js
@@ -86,6 +86,15 @@ module.exports = {
     }
   },
 
+  mock: {
+    src: {
+      mock: `${base.src}/mock/**/*`
+    },
+    dist: {
+      mock: `${base.dist}/mock`
+    }
+  },
+
   githooks: {
     src: {
       all: './tasks/githooks/*'
